470cedef89adf1c0e98d21d59b7dfcab59c13429
leeju0 <92801522+leeju0@users.noreply.github.com>
2023-08-26 22:05:02 +0900

    day9

--- src/STEP9.java
+++ src/STEP9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+class TEST4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
---
+++ src/basic_training/TEST4.java
@@ -0,0 +1,14 @@
+package basic_training;
+
+import java.util.Scanner;
+
+public class TEST4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+
+
+        System.out.println(N.length());
+    }
+}
